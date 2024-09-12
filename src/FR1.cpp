aa5d4d2c14eeae073697830e9fb8f9f8486ab75e
Frederichg <pfh3221@umoncton.ca>
2024-09-11 20:54:54 -0400

    FR1 just need to add If lever send reward

---
+++ src/FR1.cpp
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+
+#define Lever_pin 23 // Lever pin
+
+#define ENA 10  //8      // Enable pin for stepper driver
+#define DIR 5   //7      // Direction pin for stepper driver
+#define PUL 18  //6      // Pulse pin for stepper driver
+#define micro 8   // Can be 1, 2, 4, 8, 16, 32
+//11 sept 2024, STK 8 with off, off, off,  on, on, off
+
+// Function declaration
+void Reward_out();
+
+void setup() {
+  pinMode(Lever_pin, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  pinMode(PUL, OUTPUT);
+  digitalWrite(ENA, HIGH); // Disable motor
+  digitalWrite(DIR, LOW); // Clockwise when looking from top
+  digitalWrite(PUL, LOW); // Start out LOW
+  Serial.begin(115200); // Initialize serial communication
+}
+
+void loop() {
+  //Serial.print("on");
+  digitalWrite(Lever_pin, HIGH);
+  delay(2000);
+
+
+  // Call the Reward_out function
+  Reward_out();
+
+  digitalWrite(Lever_pin, LOW);
+  delay(2000);
+
+
+}
+
+void Reward_out() {
+  digitalWrite(ENA, LOW);
+  delay(100);
+  for (int x = 0; x < (200/2) * micro; x++) {
+    digitalWrite(PUL, HIGH);
+    delayMicroseconds(500 / micro);
+    digitalWrite(PUL, LOW);
+    delayMicroseconds(500 / micro);
+    delayMicroseconds(1000);  //It is a way to slow it down
+  }
+  delay(100);
+  digitalWrite(ENA, HIGH);
+  delay(1000);
+  
+}
--- src/MCP23X17.cpp
+++ src/MCP23X17.cpp
@@ -1,20 +1,32 @@
+//This one test the multiplexer
+//It test an input direct to the ESP32
+
 #include <Arduino.h>
 #include <Adafruit_MCP23X17.h>
 
-#define LED_PIN 0 // GPIO A7
+#define LED_PIN 3 // GPIO A7
+#define BUTTON_PIN 9 // 28 GPIO GPA7
 
 Adafruit_MCP23X17 mcp;
 
 void setup() {
+  Serial.begin(115200); // Initialize serial communication
   if (!mcp.begin_I2C(0x20)) { // I2C address. Can be 0x20 to 0x27, depending on the A0, A1, and A2 pins. If all 0, the address is 0x20.
     Serial.println("Error.");
     while (1);
   }
   mcp.pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // Enable internal pull-up resistor
 }
+
 void loop() {
   mcp.digitalWrite(LED_PIN, HIGH);
-  delay(500);
+  delay(50);
+  
+  if (digitalRead(BUTTON_PIN) == LOW) { // Check for LOW state
+    Serial.println("Button pressed!");
+  
   mcp.digitalWrite(LED_PIN, LOW);
-  delay(500);
+  delay(50);
+  }
 }
